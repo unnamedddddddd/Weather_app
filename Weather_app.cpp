15fc1a11d3f92b3d8e14b2b8e5c7df49cee3362e
Denis <deniskamaldinov85@gmail.com>
2025-10-05 10:07:46 +0500

    API

---
+++ Weather_app.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <curl/curl.h>
+#include <string>
+#include <Windows.h>
+#include "json.hpp"
+#include "weather_client.h"
+
+using json = nlohmann::json;
+using namespace std;
+
+int main() {
+    try {
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+
+        string city;
+        cout << u8"Введите город: "; getline(cin,city);
+        string result = PerformCURLRequest(city);
+
+        json j = json::parse(result);
+        if (j.contains("cod") && j["cod"] != 200) {
+            cout << "Ошибка API: " << j["message"] << endl;
+        }
+        cout << "Weather: " << j["name"].get<string>() << endl;
+        cout << "Cloud: " << j["weather"][0]["main"].get<string>() << endl;
+
+        return 0;
+    }
+    catch (const exception& e) {
+        cout << "Ошибка парсинга: " << e.what() << endl;
+    }
+}
+
---
+++ weather_client.h
@@ -0,0 +1,35 @@
+#pragma once
+using namespace std;
+
+size_t WriteCallBack(void* received_data_chunk, size_t size_of_element, size_t number_of_elements, string* my_result_string) {
+    size_t total_received_bytes = size_of_element * number_of_elements;
+
+    my_result_string->append((char*)received_data_chunk, total_received_bytes);
+    return total_received_bytes;
+}
+
+string CorrectURL(const string& city) {
+    string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=API&units=metric&lang=ru";
+    return url;
+}
+
+string PerformCURLRequest(const string &city) {
+    CURL* curl;
+    string result;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, CorrectURL(city).c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallBack);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
+    }
+
+    return result;
+}
