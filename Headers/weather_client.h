34803be1fa16f0e113b30222403236d2ce2e5552
Denis <deniskamaldinov85@gmail.com>
2025-10-10 15:33:51 +0500

    Final commit

--- Headers/weather_client.h
+++ Headers/weather_client.h
@@ -1,11 +1,18 @@
 #pragma once
+#include <curl/curl.h>
+#include <string>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
-size_t WriteCallBack(void* received_data_chunk, size_t size_of_element, size_t number_of_elements, string* my_result_string) {
+size_t WriteCallBack(char* received_data_chunk, size_t size_of_element, size_t number_of_elements, void* my_result_string) {
     size_t total_received_bytes = size_of_element * number_of_elements;
+    string* result = static_cast<string*>(my_result_string);
 
-    my_result_string->append(static_cast<char*>(received_data_chunk), total_received_bytes);
+    if (result && received_data_chunk) {
+        result->append(received_data_chunk, total_received_bytes);
+    }
+
     return total_received_bytes;
 }
 
@@ -22,6 +29,9 @@ string GetApiKey() {
 
 string URLEncode(const string& city) {
     string encoded;
+    if (city.empty()) {
+        return ""; 
+    }
     for (char c : city) {
         if (c == ' ') {
             encoded += "%20";
@@ -34,16 +44,14 @@ string URLEncode(const string& city) {
 }
 
 string CorrectURL(const string& city){
-    //string api = GetApiKey();
-    string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1b7dd1a7972a8cd7d1cd25a765b03a69&units=metric&lang=en";
+    string api = GetApiKey();
+    string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&appid=" + api + "&units=metric&lang=en";
     return url;
 }
 
 string PerformCURLRequest(const string &city) {
     CURL* curl;
     string result;
-
-    curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
 
     if (curl) {
@@ -51,10 +59,13 @@ string PerformCURLRequest(const string &city) {
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallBack);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
 
         curl_easy_perform(curl);
         curl_easy_cleanup(curl);
-        curl_global_cleanup();
+    }
+    else {
+     cerr << "Error: Cannot initialize CURL" << endl;
     }
 
     return result;
